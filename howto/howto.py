6c132d23aac2825cac21ccd2b20ce652d5bcd4a5
Gai <alicebeckam12345@gmail.com>
2020-03-11 00:54:01 +0800

    Update howto.py

--- howto/howto.py
+++ howto/howto.py
@@ -10,20 +10,20 @@ class Howto(commands.Cog):
     """Easy-to-use FAQ for DaGe via Google Slides"""
 
     def __init__(self, bot):
-    	self.bot = bot
+        self.bot = bot
 
     @commands.command()
     async def howto(self, ctx):
-    	"""See DaGe's How to"""
+        """See DaGe's How to"""
 
-    	thread = "Having trouble using DaGe? \n\nConfused by `>>help`? \n\nDon't worry, 'How to Use DaGe' is here!"
-     	
+        thread = "Having trouble using DaGe? \n\nConfused by `>>help`? \n\nDon't worry, 'How to Use DaGe' is here!"
+        
         faq = discord.Embed(description=thread)
-    	faq.add_field(name="Check it out on Google Slides:", value="https://bit.ly/2YMpFiW")
-    	faq.add_field(name="Annie (annie#9358) made a better Adventure guide for new adventurers here!", value"http://bit.ly/2W3atiS")
-    	faq.add_field(name="A nifty guide for customizing Dage for your server:", value="http://bit.ly/2TEVvOl")
-    	faq.set_author(name="DaGe's Visual Guide", url="https://docs.google.com/presentation/d/e/2PACX-1vSXfWU2q478KEJEcUu4eDYVTZJ3UbYupkw8Ywgk3LJbaYL2mQSeYEXeHFrY79w0Yr8F_vo7tBck_Nmc/pub?start=false&loop=false&delayms=3000")
-    	faq.set_thumbnail(url="https://cdn.discordapp.com/avatars/556752532773273639/500c9b09118178e5e7fbbec7ffbd132b.png?size=1024")
+        faq.add_field(name="Check it out on Google Slides:", value="https://bit.ly/2YMpFiW")
+        faq.add_field(name="Annie (annie#9358) made a better Adventure guide for new adventurers here!", value"http://bit.ly/2W3atiS")
+        faq.add_field(name="A nifty guide for customizing Dage for your server:", value="http://bit.ly/2TEVvOl")
+        faq.set_author(name="DaGe's Visual Guide", url="https://docs.google.com/presentation/d/e/2PACX-1vSXfWU2q478KEJEcUu4eDYVTZJ3UbYupkw8Ywgk3LJbaYL2mQSeYEXeHFrY79w0Yr8F_vo7tBck_Nmc/pub?start=false&loop=false&delayms=3000")
+        faq.set_thumbnail(url="https://cdn.discordapp.com/avatars/556752532773273639/500c9b09118178e5e7fbbec7ffbd132b.png?size=1024")
 
-    	await ctx.send(embed=faq)
+        await ctx.send(embed=faq)
         return
