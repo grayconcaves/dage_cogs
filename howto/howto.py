60ae0386d8918e997d139fcdcb92a5be2d9d30d1
Gai <alicebeckam12345@gmail.com>
2020-03-11 00:52:16 +0800

    Update howto.py

--- howto/howto.py
+++ howto/howto.py
@@ -17,11 +17,11 @@ class Howto(commands.Cog):
     	"""See DaGe's How to"""
 
     	thread = "Having trouble using DaGe? \n\nConfused by `>>help`? \n\nDon't worry, 'How to Use DaGe' is here!"
- 
-    	faq = discord.Embed(description=thread)
-    	faq.add_field(name="Check it out on Google Slides:", value="https://bit.ly/2YMpFiW")
-        faq.add_field(name="Annie (annie#9358) made a better Adventure guide for new adventurers here!", value"http://bit.ly/2W3atiS")
-        faq.add_field(name="A nifty guide for customizing Dage for your server:", value="http://bit.ly/2TEVvOl")
+     	
+        faq = discord.Embed(description=thread)
+      	faq.add_field(name="Check it out on Google Slides:", value="https://bit.ly/2YMpFiW")
+    	faq.add_field(name="Annie (annie#9358) made a better Adventure guide for new adventurers here!", value"http://bit.ly/2W3atiS")
+    	faq.add_field(name="A nifty guide for customizing Dage for your server:", value="http://bit.ly/2TEVvOl")
     	faq.set_author(name="DaGe's Visual Guide", url="https://docs.google.com/presentation/d/e/2PACX-1vSXfWU2q478KEJEcUu4eDYVTZJ3UbYupkw8Ywgk3LJbaYL2mQSeYEXeHFrY79w0Yr8F_vo7tBck_Nmc/pub?start=false&loop=false&delayms=3000")
     	faq.set_thumbnail(url="https://cdn.discordapp.com/avatars/556752532773273639/500c9b09118178e5e7fbbec7ffbd132b.png?size=1024")
 
