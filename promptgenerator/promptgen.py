0ffd8dad24f0f2949c1d5c824ca1056398e210e7
Gai <alicebeckam12345@gmail.com>
2020-04-18 13:10:06 +0800

    changed nsfw commands

--- promptgenerator/promptgen.py
+++ promptgenerator/promptgen.py
@@ -43,45 +43,58 @@ class Promptgen(commands.Cog):
 
 
     @commands.command(aliases=['prompts'])
-    async def prompt(self, ctx, numtropes = 1, flag = " ", numkinks = 1):
+    async def prompt(self, ctx, numtropes=1, flag=" ", numkinks=1):
         """Generate a romance prompt.
-        To generate a normal prompt (Universe, Challenge, Bonus), use the command without any arguments.
-        e.g. `[p]prompt`
-        
+
         To specify the number of tropes (challenges), just include a number. MAX: 5
         e.g. `[p]prompt 2`
-        To include NSFW kinks, add the 'nsfw' flag.
-        e.g. `[p]prompt 2 nsfw`
-        To include Extreme NSFW prompts, add the 'extreme' flag.
-        e.g. `[p]prompt 2 extreme`
-        To specify the number of kinks, just include a number AFTER the flag. MAX: 5
-        e.g. `[p]prompt 2 extreme 2`
         """
 
-        #data validation
+        # DATA VALIDATION
+        if numtropes < 0 or numtropes > 5:
+            return await ctx.send("Please use a number from 0-5.")
+
+        # CODE PROPER
+        prof = f"{str(random.choice(self.PROFLIST))}"
+        trope = f"Challenge: {humanize_list(random.sample(self.TROPELIST, k=numtropes))}"
+        universe = f"Universe: **{str(random.choice(self.UNIVERSELIST))} AU**"
+
+        msg = f"{universe}\n{trope}\nBonus: One of the characters is {prof}."
+
+        await ctx.send(msg)
+
+        return
+
+    @commands.command(aliases=['smutprompts'])
+    @commands.is_nsfw()
+    async def smutprompt(self, ctx, numtropes=1, numkinks=1, flag=" "):
+        """Generate a smutty romance prompt.
+
+        To specify the number of tropes (challenges), just include a number. MAX: 5
+        e.g. `[p]prompt 2`
+
+        To include Extreme NSFW kinks, add the 'extreme' flag.
+        e.g. `[p]prompt 2 extreme `
+
+        To specify the number of kinks, just include a number AFTER the number of tropes. MAX: 5
+        e.g. `[p]prompt 2 2 extreme ` or `[p]prompt 2 2`
+        """
+
+        # DATA VALIDATION
         if numtropes < 0 or numtropes > 5:
             return await ctx.send("Please use a number from 0-5.")
         if numkinks < 0 or numkinks > 5:
             return await ctx.send("Please use a number from 0-5.")
 
-        #code proper
-        if flag != " ":
-            if flag == "extreme":
-                kink = f"Kink: {humanize_list(random.sample(self.EXTREMEKINKLIST, k=numkinks))}\n"
-                prof = f"{str(random.choice(self.NSFWPROFLIST))}"
-                trope = f"Challenge: {humanize_list(random.sample(self.NSFWTROPELIST, k=numtropes))}"
-            elif flag == "nsfw":
-                kink = f"Kink: {humanize_list(random.sample(self.KINKLIST, k=numkinks))}\n"
-                prof = f"{str(random.choice(self.NSFWPROFLIST))}"
-                trope = f"Challenge: {humanize_list(random.sample(self.NSFWTROPELIST, k=numtropes))}"
-            else:
-                kink = " "
-                prof = f"{str(random.choice(self.PROFLIST))}"
-                trope = f"Challenge: {humanize_list(random.sample(self.TROPELIST, k=numtropes))}"  
+        # CODE PROPER
+        if flag == "extreme":
+            kink = f"Kink: {humanize_list(random.sample(self.EXTREMEKINKLIST, k=numkinks))}\n"
+            prof = f"{str(random.choice(self.NSFWPROFLIST))}"
+            trope = f"Challenge: {humanize_list(random.sample(self.NSFWTROPELIST, k=numtropes))}"
         else:
-            kink = " "
-            prof = f"{str(random.choice(self.PROFLIST))}"
-            trope = f"Challenge: {humanize_list(random.sample(self.TROPELIST, k=numtropes))}"    
+            kink = f"Kink: {humanize_list(random.sample(self.KINKLIST, k=numkinks))}\n"
+            prof = f"{str(random.choice(self.NSFWPROFLIST))}"
+            trope = f"Challenge: {humanize_list(random.sample(self.NSFWTROPELIST, k=numtropes))}"
         
         universe = f"Universe: **{str(random.choice(self.UNIVERSELIST))} AU**"
 
@@ -91,16 +104,18 @@ class Promptgen(commands.Cog):
 
         return
 
+
     @commands.command(aliases=['kink'])
-    async def kinks(self, ctx, numkinks = 1):
-        """NSFW! Generates a list of kinks.
+    @commands.is_nsfw()
+    async def kinks(self, ctx, numkinks=1):
+        """Generates a list of kinks.
 
         To generate a list of kinks:
         ```
         [p]kinks <number up to 6>
         """
 
-        #DATA VALIDATION
+        # DATA VALIDATION
         if numkinks < 1 or numkinks > 6:
             return await ctx.send("Please use a number from 1 to 6.")
 
@@ -111,15 +126,16 @@ class Promptgen(commands.Cog):
         return
 
     @commands.command(aliases=['extremekink'])
-    async def extremekinks(self, ctx, numkinks = 1):
-        """NSFW! Generates a list of nsfw + extreme kinks.
+    @commands.is_nsfw()
+    async def extremekinks(self, ctx, numkinks=1):
+        """Generates a list of nsfw + extreme kinks.
 
         To generate a list of nsfw + extreme kinks:
         ```
         [p]extremekinks <number up to 6>
         """
 
-        #DATA VALIDATION
+        # DATA VALIDATION
         if numkinks < 1 or numkinks > 6:
             return await ctx.send("Please use a number from 1 to 6.")
 
