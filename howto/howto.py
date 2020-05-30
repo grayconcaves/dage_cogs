1b841bb033c220fb4c31699d053952e0dbc7fa85
Gai <alicebeckam12345@gmail.com>
2020-05-31 02:44:31 +0800

    Update howto.py

--- howto/howto.py
+++ howto/howto.py
@@ -16,13 +16,12 @@ class Howto(commands.Cog):
     async def howto(self, ctx):
         """See DaGe's How to"""
 
-        thread = "Having trouble using DaGe? \n\nConfused by `>>help`? \n\nDon't worry, 'How to Use DaGe' is here!"
+        thread = "Having trouble using DaGe? \n\nConfused by `>>help`? \n\nDon't worry, 'DaGe's Docs' is here!"
         
         faq = discord.Embed(description=thread)
-        faq.add_field(name="Check it out on Google Slides:", value="https://bit.ly/2YMpFiW")
+        faq.add_field(name="Check it out on Google Docs:", value="https://bit.ly/dagesdocs")
         faq.add_field(name="Annie (annie#9358) made a better Adventure guide for new adventurers here!", value="http://bit.ly/2W3atiS")
-        faq.add_field(name="A nifty guide for customizing Dage for your server:", value="http://bit.ly/2TEVvOl")
-        faq.set_author(name="DaGe's Visual Guide", url="https://docs.google.com/presentation/d/e/2PACX-1vSXfWU2q478KEJEcUu4eDYVTZJ3UbYupkw8Ywgk3LJbaYL2mQSeYEXeHFrY79w0Yr8F_vo7tBck_Nmc/pub?start=false&loop=false&delayms=3000")
+        faq.set_author(name="DaGe's Doc's", url="https://bit.ly/dagesdocs")
         faq.set_thumbnail(url="https://cdn.discordapp.com/avatars/556752532773273639/500c9b09118178e5e7fbbec7ffbd132b.png?size=1024")
 
         await ctx.send(embed=faq)
