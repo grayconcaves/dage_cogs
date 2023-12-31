6aca78f885e34df20971d4b0f1f1e4cc47576167
Gai <alicebeckam12345@gmail.com>
2023-12-31 15:42:57 +0800

    Update __init__.py

--- ao3/__init__.py
+++ ao3/__init__.py
@@ -1,5 +1,6 @@
 from .ao3 import Ao3
 
+__red_end_user_data_statement__ = "This cog does not persistently store data or metadata about users."
 
-def setup(bot):
-    bot.add_cog(Ao3(bot))
+async def setup(bot):
+    await bot.add_cog(Ao3(bot))
