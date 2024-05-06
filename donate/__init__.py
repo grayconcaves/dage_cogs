ae6cb84f3f2a363eec4ce1039a33d9eadca4f0da
Gai <alicebeckam12345@gmail.com>
2024-05-06 19:43:10 +0800

    Update __init__.py

--- donate/__init__.py
+++ donate/__init__.py
@@ -3,4 +3,4 @@ from .donate import Donate
 __red_end_user_data_statement__ = "This cog does not persistently store data or metadata about users."
 
 async def setup(bot):
-    bot.add_cog(Donate(bot))
+    await bot.add_cog(Donate(bot))
