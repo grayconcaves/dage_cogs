3e7bedf093592886509e483d2238d0d922b926f6
Gai <alicebeckam12345@gmail.com>
2024-05-06 19:42:51 +0800

    Update __init__.py

--- cnoveltags/__init__.py
+++ cnoveltags/__init__.py
@@ -3,4 +3,4 @@ from .cnoveltags import Cnoveltags
 __red_end_user_data_statement__ = "This cog does not persistently store data or metadata about users."
 
 async def setup(bot):
-    bot.add_cog(Cnoveltags(bot))
+    await bot.add_cog(Cnoveltags(bot))
