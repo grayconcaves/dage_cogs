850c8e48ef12a005ccdc931cd21a290f7a486f66
Gai <alicebeckam12345@gmail.com>
2024-05-06 19:43:28 +0800

    Update __init__.py

--- howto/__init__.py
+++ howto/__init__.py
@@ -3,4 +3,4 @@ from .howto import Howto
 __red_end_user_data_statement__ = "This cog does not persistently store data or metadata about users."
 
 async def setup(bot):
-    await.add_cog(Howto(bot))
+    await bot.add_cog(Howto(bot))
